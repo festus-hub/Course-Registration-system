8d9902de012391ec28550cb5a0539759515b37f2
Festus <106164588+festus-hub@users.noreply.github.com>
2026-06-30 19:57:34 +0300

    updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
+
+def landing_page(request):
+    return render(request, 'landing.html')
+
+# Register View
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect('home')      
+
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, 'accounts/login.html')
+
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have logged out successfully.")
+    return redirect('landing')
